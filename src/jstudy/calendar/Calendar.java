798b8c8d9a7e4d0dfb37a4bf7d0c6c069613cf97
shinyhope <seo.kyung.hee@gmail.com>
2019-10-24 09:39:55 +0900

    월을 입력 받고 해당 월의 최대일수를 출력

--- src/jstudy/calendar/Calendar.java
+++ src/jstudy/calendar/Calendar.java
@@ -1,10 +1,20 @@
 package jstudy.calendar;
 
+import java.util.Scanner;
 
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("hello, calendar");
+				
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("값을 입력하세요.");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d 월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		scanner.close();
+		
 	}
 
 }
