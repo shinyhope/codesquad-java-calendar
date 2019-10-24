ab3a4d0fc05a703d767474bba92bff95b199f3e8
shinyhope <seo.kyung.hee@gmail.com>
2019-10-24 10:04:51 +0900

    입력 횟수를 for구문을 사용해서 구현

--- src/jstudy/calendar/Calendar.java
+++ src/jstudy/calendar/Calendar.java
@@ -3,24 +3,29 @@ package jstudy.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month -1];
+		return MAX_DAYS[month - 1];
 	}
 
 	public static void main(String[] args) {
-				
+
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("값을 입력하세요.");
-		int month = scanner.nextInt();		
-	
-		
-		System.out.printf("%d 월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요.");
+			int month = scanner.nextInt();
+			System.out.printf("%d 월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println("bye.");
 		scanner.close();
-		
+
 	}
 
 }
